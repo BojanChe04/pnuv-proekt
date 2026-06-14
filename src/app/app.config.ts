a558625591a34ba3eb9e20cd1a15bbee6a5f1c55
SimonaN <nastoskasimona40@gmail.com>
2026-06-14 13:44:28 +0200

    Remove login/register buttons when logged in

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withRouterConfig  } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Auth, onAuthStateChanged } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,12 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class HomeComponent {}
+export class HomeComponent {
+    isLoggedIn = false;
+    constructor(private auth: Auth) {
+      // Firebase автоматски проверува дали корисникот е најавен
+      onAuthStateChanged(auth, (user) => {
+      this.isLoggedIn = !!user; // true ако е најавен, false ако не е
+    });
+  }
+}
